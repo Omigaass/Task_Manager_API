9bb46979011f806196ca161a17e645e76ea27c5f
edxlty <eduardosjr06@gmail.com>
2024-10-19 00:04:19 -0300

    API - Read

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -17,3 +17,13 @@ export const addUser = (req, res) => {
       return res.status(200).json("Usuário criado com sucesso.");
     });
   };
+  
+  export const getUsers = (_, res) => {
+    const q = "SELECT * FROM usuarios";
+  
+    db.query(q, (err, data) => {
+      if (err) return res.json(err);
+  
+      return res.status(200).json(data);
+    });
+  };
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { } from "../controllers/user.js";
+import { addUser, getUsers } from "../controllers/user.js";
 
 const router = express.Router()
 
 router.post("/", addUser)
+
+router.get("/", getUsers)
+
 
 export default router
